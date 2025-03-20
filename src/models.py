f69fc26ac392d22c64b0097b633c4c1e3aa533ea
Maksim <kekswat@mail.ru>
2025-03-21 06:51:55 +0700

    feat: test postgres

--- src/models.py
+++ src/models.py
@@ -4,7 +4,7 @@ from database import Base
 
 class RecipeModel(Base):
     __tablename__ = "recipes"
-    __table_args__ = {'extend_existing': True}
+    #__table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     views = Column(Integer, default=0)
