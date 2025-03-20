f639ef9cb3d8af90a273fc905cda590675587e50
Maksim <kekswat@mail.ru>
2025-03-21 06:46:01 +0700

    feat: test postgres

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,6 @@ TEST_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5433/test_
 async def async_db_engine():
     engine = create_async_engine(TEST_DATABASE_URL, echo=True)
     async with engine.begin() as conn:
-        #Base.metadata.clear()
         await conn.run_sync(Base.metadata.create_all)
     yield engine
     await engine.dispose()
@@ -24,11 +23,6 @@ async def async_db_session(async_db_engine):
     async_session = sessionmaker(bind=async_db_engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         yield session
-
-@pytest.fixture(autouse=True)
-async def cleanup_metadata():
-    Base.metadata.clear()  # Очистка метаданных SQLAlchemy
-    yield
 
 
 @pytest.fixture()
