cfcd7f6675af55b3c063d2a4c8ead39988690f6a
Maksim <kekswat@mail.ru>
2025-03-22 02:34:02 +0700

    feat: test postgres

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,47 +2,30 @@ import pytest
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 from src.database import Base
+from src.main import app
 
 TEST_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5433/test_db"
-
-
-@pytest.fixture(scope="session")
-async def async_db_engine():
-    engine = create_async_engine(TEST_DATABASE_URL, echo=True)
-    yield engine
-    await engine.dispose()
-
+engine = create_async_engine(TEST_DATABASE_URL, echo=True, poolclass=NullPool)
+async_session = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
 
 @pytest.fixture(scope="session")
-async def init_test_db(async_db_engine):
-    async with async_db_engine.begin() as conn:
+async def init_test_db():
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
-    async with async_db_engine.begin() as conn:
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
 
 @pytest.fixture()
-async def async_db_session(async_db_engine):
-    async_session = sessionmaker(bind=async_db_engine, expire_on_commit=False, class_=AsyncSession)
-    async with async_session() as session:
-        yield session
-        await session.rollback()
-
-
-@pytest.fixture()
-def app():
-    from src.main import app
-    yield app
-
-
-@pytest.fixture()
-async def client(async_db_session, app, init_test_db):
+async def client(init_test_db):
     from src.main import get_db, init_db
 
     async def override_get_db():
-        yield async_db_session
+        async with async_session() as session:
+            yield session
 
     app.dependency_overrides[get_db] = override_get_db
     app.dependency_overrides[init_db] = init_test_db
