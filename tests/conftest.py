75c78da3b1d50ea42e5e9881e5c20fea7cf73c62
Maksim <kekswat@mail.ru>
2025-03-22 02:17:10 +0700

    feat: test postgres

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
-import asyncio
 import pytest
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from src.models import Base
-
+from src.database import Base
 
 TEST_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5433/test_db"
 
@@ -12,35 +10,43 @@ TEST_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5433/test_
 @pytest.fixture(scope="session")
 async def async_db_engine():
     engine = create_async_engine(TEST_DATABASE_URL, echo=True)
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield engine
     await engine.dispose()
 
 
 @pytest.fixture(scope="session")
+async def init_test_db(async_db_engine):
+    async with async_db_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with async_db_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture()
 async def async_db_session(async_db_engine):
     async_session = sessionmaker(bind=async_db_engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         yield session
+        await session.rollback()
 
 
 @pytest.fixture()
 def app():
     from src.main import app
-
     yield app
 
 
 @pytest.fixture()
-async def client(async_db_session, app):
+async def client(async_db_session, app, init_test_db):
+    from src.main import get_db, init_db
+
+    async def override_get_db():
+        yield async_db_session
+
+    app.dependency_overrides[get_db] = override_get_db
+    app.dependency_overrides[init_db] = init_test_db
+
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as client:
         yield client
-
-
-@pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
