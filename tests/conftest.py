92911851c746fc564ff964165ef9127e0adc5032
Maksim <kekswat@mail.ru>
2025-03-22 15:40:44 +0700

    fix: fix isort exceptions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from httpx import ASGITransport, AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.pool import NullPool
 
 from src.main import app, get_db
