496ea6d682d492acfa69ecaf54045f712421fad7
Maksim <kekswat@mail.ru>
2025-03-22 14:55:09 +0700

    fix: fix imports, metadata sqlalchemy

--- src/models.py
+++ src/models.py
@@ -4,7 +4,6 @@ from src.database import Base
 
 class RecipeModel(Base):
     __tablename__ = "recipes"
-    #__table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     views = Column(Integer, default=0)
